a42076119277f96d9e3104236522a31db2c027ff
gheezl <gheezl25@gmail.com>
2020-09-03 18:21:42 -0400

    Started filling out the input page

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 const App = () => {
+  let [input, setInput] = useState("")
+  let [toggle, setToggle] = useState(false)
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Hi</Text>
       <Text style={styles.text} >What is your name?</Text>
+      <TextInput
+        placeholder="Enter your name"
+        value={input}
+        style={styles.input}
+        onChangeText={text => setInput(text)}
+      />
+      {
+        input
+          ? (
+            <TouchableOpacity onPress={() => setToggle(true)}>
+              <Text style={styles.button} >Submit name</Text>
+            </TouchableOpacity>)
+          : (null)
+      }
+      {
+        toggle
+          ? (<Text style={styles.text} >Welcome {input}</Text>)
+          : (null)
+      }
       <StatusBar style="auto" />
     </View>
   );
@@ -23,6 +45,30 @@ const styles = StyleSheet.create({
   text: {
     color: "blue",
     fontSize: 25,
+  },
+
+  input: {
+    marginTop: 15,
+    height: 35,
+    width: 275,
+    borderColor: "black",
+    borderWidth: 1,
+    borderRadius: 100 / 2,
+    textAlign: "center",
+    fontSize: 15,
+  },
+
+  button: {
+    padding: 5,
+    marginTop: 15,
+    height: 35,
+    width: 275,
+    borderColor: "black",
+    backgroundColor: "lightblue",
+    borderWidth: 1,
+    borderRadius: 100 / 2,
+    textAlign: "center",
+    fontSize: 15,
   }
 });
 
